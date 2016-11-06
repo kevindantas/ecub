4074f3e2d06327525ba44e300b5a2280ca8453fb
Kevin-ds <kevin.ds.29@gmail.com>
2016-11-06 14:14:40 -0200

    Highlight menu item on scroll

--- js/app.js
+++ js/app.js
@@ -4,13 +4,10 @@ function clickMenu (e) {
 
 	this.classList.add('active');
 }
-var menuItems = document.querySelectorAll('.navbar li');
-for (var i = menuItems.length - 1; i >= 0; i--) {
-	menuItems[i].addEventListener('click', clickMenu)
-}
 
-
-
+var menu = document.querySelector('.navbar .menu');
+var menuAnimate  = document.querySelector('.navbar .menu-animate');
+var menuItems = document.querySelectorAll('.navbar .menu li');
 var sections = [
 	document.querySelector('#app'),
 	document.querySelector('#baterias'),
@@ -19,24 +16,53 @@ var sections = [
 	document.querySelector('#design'),
 ];
 
-function handleScroll(e) {
-	for (var i = sections.length - 1; i >= 0; i--) {
-		sections[i]
-		if(calcPos(window.scrollY, sections[i])){
-			changeMenu(sections[i].getAttribute('id'));
-			return;
-		}
-	}
+
+for (var i = menuItems.length - 1; i >= 0; i--) {
+	menuItems[i].addEventListener('click', clickMenu)
 }
 
 
-function changeMenu(id) {
-	console.log(id)
+
+console.log(menu.offsetLeft)
+menuAnimate.style.width = menuItems[0].offsetWidth+'px';
+menuAnimate.style.transform = 'translateX('+menu.offsetLeft+'px)';
+
+function handleScroll(e) {
+	
+	
+	for (var i = sections.length - 1; i >= 0; i--) 
+		menuItems[(menuItems.length-1) - i].classList.remove('active');		
+	
+	for (var i = sections.length - 1; i >= 0; i--) {
+		if(calcPos(window.scrollY, sections[i])){
+			changeMenu(i);
+			return;
+		}
+	}
+	
+	if(i != sections.length)
+		menuAnimate.style.opacity = 0;
+	
+	
+}
+
+
+function changeMenu(index) {
+	
+	var itemLeft = menuItems[(menuItems.length-1) - index].offsetLeft;
+	var menuLeft = menu.offsetLeft;
+	menuItems[(menuItems.length-1) - index].classList.add('active');
+	menuAnimate.style.width = menuItems[(menuItems.length-1) - index].offsetWidth+'px';
+	menuAnimate.style.transform = 'translateX('+(menuLeft + itemLeft)+'px)';
+	menuAnimate.style.opacity = 1;
+
+	
+	menuItems[(menuItems.length-1) - i]
 }
 
 
 function calcPos(scrollPos, element) {
-	return scrollPos > (element.offsetTop*0.8) && scrollPos < element.offsetTop+element.offsetHeight
+	return scrollPos > (element.offsetTop-200) && scrollPos < element.offsetTop-200+element.offsetHeight
 }
 
 window.addEventListener('scroll', handleScroll)
